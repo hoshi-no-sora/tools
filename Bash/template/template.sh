80657adef68d0bf1c2e4b24feb7077e1afc2ba13
hoshi-no-sora <52352309+hoshi-no-sora@users.noreply.github.com>
2019-09-16 23:47:15 +0900

    [Put] Bash script template

---
+++ Bash/template/template.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+#===============================================
+# [ Explanation ]
+# 
+# objective: ---
+#           
+# format: bash .sh [arg1] [arg2]
+# arg1: [explaination of arg1]
+#       "nothing" : display help
+#       --help    : display help
+#       --status  : display ps status
+#
+# arg2: [explaination of arg2]
+# 
+#
+# [ ※※※ Caution ※※※ ]
+# 
+#
+# [ Info ]
+#
+# Not Set "LOGFILE" variable => ${HOME}/program.log
+# 
+#===============================================
+
+#==============================================#
+#=== [ Variable and Defalut Value Setting ] ===#
+#==============================================#
+LOGDIR=
+
+#==============================================#
+#=== [ Variable and Defalut Value Setting ] ===#
+#==============================================#
+
+######################################################
+
+#===============================================
+#========== [ Environmental Setting ] ==========
+#===============================================
+set -e -o pipefail -o noclobber -o ignoreeof
+export MYTOOLDIR="${HOME}/TOOLS/Bash"
+source "${MYTOOLDIR}/share/error_func"
+source "${MYTOOLDIR}/share/share_func"
+source "${MYTOOLDIR}/share/debug_func"
+source "${MYTOOLDIR}/share/coloring_func"
+#===============================================
+#========== [ Environmental Setting ] ==========
+#===============================================
+
+#===============================================
+#========= [ Definition of function ] =========#
+#=============================================== 
+
+# [ definition of help message format ]
+function help() {
+cat << EOF
+=========================== [ USAGE ] ===========================
+objective: "[purpose of script]"
+format   : $(basename $0) [arg1] [arg2]
+--------------------
+arg1: "[arg1 explanation]"
+arg2: "[arg2 explanation]"
+=========================== [ USAGE ] ===========================
+EOF
+}
+
+function () {
+    
+}
+
+# function (){
+# 
+# }
+
+#===============================================
+#========= [ Definition of function ] =========#
+#===============================================
+
+# [ processing or display help ]
+argument_check $(basename $0)   # inside share_func 
+
+
+[ -z  ] || error ""
+[ -d  ] || abort "no such directory"
+
+
+exit
+######################################################
+#
+#
+
